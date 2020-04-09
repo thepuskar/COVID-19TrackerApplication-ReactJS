7f264c83d980750a45d8734a94a6079137b642a2
devpuskar <puskar.ad@gmail.com>
2020-04-09 05:58:21 +0545

    basic style added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./App.css";
+
 import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
@@ -7,7 +9,7 @@ import CountryPicker from "./components/CountryPicker/CountryPicker";
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <Cards />
         <Chart />
         <CountryPicker />
