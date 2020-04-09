d288a5228a90e2be90b01800ddf5a086c8eb57bb
devpuskar <puskar.ad@gmail.com>
2020-04-09 06:07:04 +0545

    api has been called and data is fetched

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
 
+import { fetchData } from "./api";
+
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className="container">
---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const url = "https://covid19.mathdro.id/api";
+
+export const fetchData = async () => {
+  try {
+    const response = await axios.get(url);
+    return response;
+  } catch (error) {}
+};
+
+export default fetchData;
