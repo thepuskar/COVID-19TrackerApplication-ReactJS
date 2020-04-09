628b5d278dbfab2b6a74034b5fe8cd3794f4c77e
devpuskar <puskar.ad@gmail.com>
2020-04-09 06:13:47 +0545

    data has get from api

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,18 @@ const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get(url);
-    return response;
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+
+    const modifiedData = {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
+    };
+
+    return modifiedData;
   } catch (error) {}
 };
 
