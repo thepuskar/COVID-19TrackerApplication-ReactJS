475451d649ef056f27f7c9c66848758d68458b01
devpuskar <puskar.ad@gmail.com>
2020-04-09 11:31:21 +0545

    footer is completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,24 +4,23 @@ import "./Footer.style.scss";
 class Footer extends React.Component {
   render() {
     return (
-      <div className="pg-footer">
-        <footer className="footer">
-          <div className="footer-copyright">
-            <div className="footer-copyright-wrapper">
-              <p className="footer-copyright-text">
-                <a
-                  className="footer-copyright-link"
-                  href="https://npooja.now.sh/"
-                  target="_self"
-                >
-                  {" "}
-                  ©2020. | Designed and Developed By Puskar Adhikari. | All rights reserved.{" "}
-                </a>
-              </p>
-            </div>
+      <footer className="footer">
+        <div className="footer-copyright">
+          <div className="footer-copyright-wrapper">
+            <p className="footer-copyright-text">
+              <a
+                className="footer-copyright-link"
+                href="https://twitter.com/dev_puskar_"
+                target="_self"
+              >
+                {" "}
+                ©{new Date().getFullYear()} | Designed and Developed By Puskar
+                Adhikari. | All rights reserved.{" "}
+              </a>
+            </p>
           </div>
-        </footer>
-      </div>
+        </div>
+      </footer>
     );
   }
 }
